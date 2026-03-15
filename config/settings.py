a606524bf8454bc31413b9846c9fb0fe4827d4a6
Chirayu <114068521+ckbadgujar2003@users.noreply.github.com>
2026-03-15 10:52:38 +0530

    Secured settings.py using .env

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,13 @@
-CHROMEDRIVER_LOC="C:/Users/your-path-to/chromedriver-win64/chromedriver.exe"
-SENDER_MAIL="sender@email.com
-SENDER_MAIL_PASSWORD="abcd efgh ijkl mnop"
-RECEIVER_MAIL="receiver@email.com"
-SMTP_HOST="smtp.gmail.com"
-SMTP_PORT=587
+import os
+
+CHROMEDRIVER_LOC = os.getenv("CHROMEDRIVER_LOC")
+
+SENDER_MAIL = os.getenv("SENDER_MAIL")
+SENDER_MAIL_PASSWORD = os.getenv("SENDER_MAIL_PASSWORD")
+
+RECEIVER_MAIL = os.getenv("RECEIVER_MAIL")
+
+SMTP_HOST = os.getenv("SMTP_HOST", "smtp.gmail.com")
+SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
 
 USER_AGENT = "NeoSpyder/2.0 (Defensive Security Research)"
