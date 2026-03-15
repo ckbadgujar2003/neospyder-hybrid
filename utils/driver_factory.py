0aaff579d5f5caf50e554d045088fed94b04a7bf
Chirayu <114068521+ckbadgujar2003@users.noreply.github.com>
2026-03-15 12:00:39 +0530

    added missing Linux CI safety flags and Chromium binary path detection

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,23 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+import shutil
 import os
-import sys
 
 
 def get_driver():
+
     chrome_options = Options()
 
     chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--log-level=3")
-    chrome_options.add_argument("--silent")
+    chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--disable-logging")
+    chrome_options.add_argument("--log-level=3")
+    chrome_options.add_argument("--window-size=1920,1080")
 
-    # suppress selenium logs
-    service = Service(
-        log_path=os.devnull
-    )
+    # Detect chromium location (GitHub runner)
+    chrome_binary = shutil.which("chromium-browser") or shutil.which("chromium")
+
+    if chrome_binary:
+        chrome_options.binary_location = chrome_binary
+
+    service = Service(log_path=os.devnull)
 
     driver = webdriver.Chrome(
         service=service,
